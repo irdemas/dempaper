6a5992a1a3ad6a10c1cd2f246793783c506d776f
irdemas <48674271+irdemas@users.noreply.github.com>
2020-11-27 23:09:30 +0700

    Update test_package.py

--- test_package.py
+++ test_package.py
@@ -1,5 +1,5 @@
 import os
 
-path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
 
 print(path)
